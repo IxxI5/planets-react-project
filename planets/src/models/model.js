390c052acd348de8daba717e190db516203b4c43
IxxI5 <ioannisxydous@gmail.com>
2023-02-07 16:56:30 +0100

    model.js: planet and resident models added

---
+++ planets/src/models/model.js
@@ -0,0 +1,20 @@
+// Planet Model
+let planet = {
+  id: 0,
+  name: "",
+  diameter: "",
+  gravity: "",
+  terrain: "",
+  population: "",
+  residents: [],
+};
+
+// Notable Resident Model
+let resident = {
+  name: "",
+  height: "",
+  birth_year: "",
+  gender: "",
+};
+
+export default { planet, resident };
